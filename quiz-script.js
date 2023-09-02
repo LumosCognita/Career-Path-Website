ba5b40eaa70cf040cb2b23f5f70860035f984223
Najem-Nabhani <nabhaninajm@gmail.com>
2023-09-02 14:20:38 +0300

    Call /quiz API

--- quiz-script.js
+++ quiz-script.js
@@ -1,3 +1,5 @@
+window.onload = () => {
+
 const main = document.querySelector('main');
 const header = document.querySelector('header');
 
@@ -41,132 +43,122 @@ main.style.height = `calc(100vh - ${headerHeight.height}px)`;
 
 /* Saving the user choices into a JSON object and setting the counter */
 
-const answerOptions = document.querySelector('#answerOptions');
-const nextBTN = document.querySelector('.nextBTN');
-let questionNumber = document.querySelector('.questionNumber');
-let counter = 2;
+    const answerOptions = document.querySelector('#answerOptions');
+    const nextBTN = document.querySelector('.nextBTN');
+    let questionNumber = document.querySelector('.questionNumber');
+    let counter = 2;
 
-nextBTN.addEventListener('click', () => {
-    const question = document.querySelector('.questionText');
-    const selectedValue = answerOptions.value;
-    if (selectedValue !== '---') {
-        const userChoice = {
-            'question': question.textContent,
-            'userAnswer': selectedValue
-        };
-        console.log(userChoice);
-    }
+    let questions = [];
+    let submittedQuestions = [];
+    let questionIndex = 1;
 
-    if (questionNumber.innerHTML < 15 && selectedValue !== '---') {
-        if (counter < 10) {
-            questionNumber.innerHTML = `0${counter}`;
+    nextBTN.addEventListener('click', () => {
+        const selectedIndex = answerOptions.selectedIndex;
+        console.debug({selectedValue: selectedIndex});
+        if (selectedIndex === 0) {
+            return;
         }
 
-        else {
-            questionNumber.innerHTML = `${counter}`;
+        //Store the answer to prev. question
+        submittedQuestions.push({
+            ...questions[questionIndex-1], 
+            "user_answer": selectedIndex
+        });
+        console.debug({submittedQuestions});
+
+        if (questionNumber.innerHTML < 15) {
+            if (counter < 10) {
+                questionNumber.innerHTML = `0${counter}`;
+            }
+
+            else {
+                questionNumber.innerHTML = `${counter}`;
+            }
+
+            counter++;
+            changeQuestion();
         }
-
-        counter++;
-        changeQuestion();
-    }
-})
-
-const questionText = document.querySelector('.questionText');
-
-function typingEffect(typewriter) {
-    questionText.textContent = "";
-
-    let i = 0;
-    function typewriterEffect() {
-        if (i < typewriter.length) {
-            questionText.textContent += typewriter.charAt(i);
-            i++;
-            setTimeout(typewriterEffect, 50);
-        }
-    }
-
-    typewriterEffect();
-}
-
-function typingEffectAnswers(answersList) {
-    const answerSlots = document.querySelectorAll('.answersList li');
-    let currentAnswerIndex = 0;
-    let currentCharacterIndex = 0;
-
-    answerSlots.forEach((slot) => {
-        slot.textContent = "";
     })
 
-    function typewriterEffect() {
-        if (currentAnswerIndex < answersList.length) {
-            const slotText = answerSlots[currentAnswerIndex];
-            const currentAnswer = answersList[currentAnswerIndex];
-            const { label } = currentAnswer;
+    const questionText = document.querySelector('.questionText');
 
-            if (currentCharacterIndex < label.length) {
-                slotText.textContent += label.charAt(currentCharacterIndex);
-                currentCharacterIndex++;
-                setTimeout(typewriterEffect, 25);
-            } else {
-                currentAnswerIndex++;
-                currentCharacterIndex = 0;
-                setTimeout(typewriterEffect, 1000); // Wait for 1 second before moving to the next answer
+    function typingEffect(typewriter) {
+        questionText.textContent = "";
+
+        let i = 0;
+        function typewriterEffect() {
+            if (i < typewriter.length) {
+                questionText.textContent += typewriter.charAt(i);
+                i++;
+                setTimeout(typewriterEffect, 50);
             }
+        }
+
+        typewriterEffect();
+    }
+
+    function typingEffectAnswers(answersList) {
+        const answerSlots = document.querySelectorAll('.answersList li');
+        let currentAnswerIndex = 0;
+        let currentCharacterIndex = 0;
+
+        answerSlots.forEach((slot) => {
+            slot.textContent = "";
+        })
+
+        function typewriterEffect() {
+            if (currentAnswerIndex < answersList.length) {
+                const slotText = answerSlots[currentAnswerIndex];
+                const currentAnswer = answersList[currentAnswerIndex];
+                const { label } = currentAnswer;
+
+                if (currentCharacterIndex < label.length) {
+                    slotText.textContent += label.charAt(currentCharacterIndex);
+                    currentCharacterIndex++;
+                    setTimeout(typewriterEffect, 25);
+                } else {
+                    currentAnswerIndex++;
+                    currentCharacterIndex = 0;
+                    setTimeout(typewriterEffect, 1000); // Wait for 1 second before moving to the next answer
+                }
+            }
+        }
+
+        typewriterEffect(); // Start the typewriter effect
+    }
+
+    function changeQuestion() {
+        if (questionIndex < questions.length) {
+            typingEffect(questions[questionIndex].question);
+            typingEffectAnswers(questions[questionIndex].options);
+            questionIndex++;
+        } else {
+            localStorage.setItem("questions", JSON.stringify({"questions": submittedQuestions}));
+            const locationPath = window.location.href.split("/");
+            locationPath.pop();
+            locationPath.push("course_info.html");
+            window.location.href = locationPath.join("/");
         }
     }
 
-    typewriterEffect(); // Start the typewriter effect
-}
+    function showTheFirstQuestion() {
+        fetch('http://localhost:8000/get_quiz?user_major=SE')
+            .then(function (response) {
+                return response.json()
+            })
+            .then(function (data) {
+                questions = data.questions;
+                console.log({questions});
+                if (questionIndex < data.questions.length) {
+                    typingEffect(data.questions[0].question);
+                    typingEffectAnswers(data.questions[0].options);
+                }
+                console.log(data.questions[0].options[0].label);
+            })
+            .catch(function (error) {
+                console.error('Failed to fetch quiz:', error);
+            })
+    }
 
-
-
-let questionIndex = 1;
-
-function changeQuestion() {
-    fetch('questions.json')
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            if (questionIndex < data.questions.length) {
-                typingEffect(data.questions[questionIndex].question);
-                typingEffectAnswers(data.questions[questionIndex].options);
-                questionIndex++;
-            }
-        })
-}
-
-function showTheFirstQuestion() {
-    fetch('questions.json')
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            if (questionIndex < data.questions.length) {
-                typingEffect(data.questions[0].question);
-                typingEffectAnswers(data.questions[0].options);
-            }
-            console.log(data.questions[0].options[0].label);
-        })
-}
-
-showTheFirstQuestion();
-
-
-// const questions = [
-//     'What\'s your name?',
-//     'How old are you?',
-//     'Where do you live?'
-// ];
-
-// const questions2 = {
-//     'q1': {
-//         'qText': 'What is your name',
-//         'answers': ['Adam', 'Ali', 'Idrees', 'Rami']
-//     },
-
-//     'q2': {
-//         'qText': 'How are you?',
-//         'answers': ['Good', 'Perfect', 'Excellent', 'Relaxed']
-//     }
-// };
+    showTheFirstQuestion();
+};
